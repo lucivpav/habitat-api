83baa3a5cf7f1d357cee3d51e6df5cd66f35267e
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-04-12 16:33:20 +0200

    generate poses.csv

--- examples/capture_sequence.py
+++ examples/capture_sequence.py
@@ -2,14 +2,23 @@ import habitat
 import cv2
 import os.path
 import habitat_sim
+from habitat_sim.utils.common import quat_from_magnum
+import quaternion
+import numpy as np
 
 from habitat.sims.habitat_simulator.actions import HabitatSimActionsSingleton
 
 def transform_rgb_bgr(image):
     return image[:, :, [2, 1, 0]]
 
-def example():
+def capture_sequence():
     outputDir = '/Volumes/GoogleDrive/Můj disk/ARTwin/personal/lucivpav/habitat'
+    posesDir = os.path.join(outputDir, 'poses')
+    posesPath = os.path.join(posesDir, 'poses.csv')
+
+    if not os.path.isdir(posesDir):
+        os.mkdir(posesDir)
+
     env = habitat.Env(config=habitat.get_config("configs/datasets/pointnav/mp3d.yaml"))
 
     print("Environment creation successful")
@@ -17,6 +26,8 @@ def example():
     cv2.imshow("RGB", transform_rgb_bgr(observations["rgb"]))
 
     print("Agent stepping around inside environment.")
+    posesFile = open(posesPath, 'w')
+    posesFile.write('id x y z dirx diry dirz\n')
 
     count_steps = 0
     while not env.episode_over:
@@ -46,19 +57,30 @@ def example():
 
         observations = env.step(action)
         count_steps += 1
-        cv2.imwrite(os.path.join(outputDir, f'export_imgs/im_{count_steps}.png'), transform_rgb_bgr(observations["rgb"]))
 
+        state = env._sim.get_agent_state(0)
+        rotation1 = state.rotation
+        rotation2 = state.sensor_states['rgb'].rotation
+        rotation = rotation1 * rotation2
+        print(f'Position: {state.position}')
+        x = state.position[0]
+        y = state.position[1]
+        z = state.position[2]
+        R = quaternion.as_rotation_matrix(rotation)
+        initialCameraDirection = np.array([0.0, 0.0, -1.0]).T
+        cameraDirection = R @ initialCameraDirection
+        dirx = cameraDirection[0]
+        diry = cameraDirection[1]
+        dirz = cameraDirection[2]
+        print(f'Rotation: {dirx},{diry},{dirz}')
+        posesFile.write('%0.2f %0.2f %0.2f %0.2f %0.2f %0.2f\n' % (x,y,z, dirx,diry,dirz))
+
+        cv2.imwrite(os.path.join(posesDir, f'{count_steps}.png'), transform_rgb_bgr(observations["rgb"]))
         cv2.imshow("RGB", transform_rgb_bgr(observations["rgb"]))
 
-    print("Episode finished after {} steps.".format(count_steps))
-
-    if action == HabitatSimActions.STOP and observations["pointgoal_with_gps_compass"][0] < 0.2:
-        print("you successfully navigated to destination point")
-    else:
-        print("your navigation was unsuccessful")
-
+    posesFile.close()
 
 if __name__ == "__main__":
     print(os.path.abspath('./'))
     HabitatSimActions: HabitatSimActionsSingleton = HabitatSimActionsSingleton()
-    example()
+    capture_sequence()
